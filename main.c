99d1f39c4ebcf9658dc27d6d0000970d547fd598
Roniel Nunes <roniel.barbosa@ufv.br>
2021-03-28 21:41:34 -0300

    Arquivos de entrada

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+*   Programa: PROBLEMA DO LABIRINTO
+*   Autor: Roniel Nunes Barbosa e Victor Hugo Santos
+*   Matéria: Projeto e Análise de Algoritmos
+*   Professor: Daniel Mendes Barbosa
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
